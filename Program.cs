8909fb96953cbb248774b806654aaa6fe16af15a
Rodrigo Alves <rodrigorac.2003@gmail.com>
2021-04-06 13:23:23 -0300

    Cálculo de FGTS

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace FGTS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+
+
+
+
+
+            Double sal;
+            Double Resul;
+
+            Console.WriteLine();
+            Console.WriteLine("     FGTS");
+            Console.WriteLine();
+            Console.WriteLine("--> Digite o valor a ser calculado.");
+
+            sal = Convert.ToDouble (Console.ReadLine());
+
+            Resul = (sal*8) / 100;
+
+            Console.WriteLine();
+            Console.WriteLine($"Salário: {sal:C2}");
+            Console.WriteLine($"FGTS: {Resul:C2}");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
